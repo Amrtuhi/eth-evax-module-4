22a51e62277a4908465362e2c50023b0f93f960f
Amrita kumari <149098850+Amrtuhi@users.noreply.github.com>
2024-08-20 15:16:03 +0530

    degentoken.sol

--- degentoken.sol
+++ degentoken.sol
@@ -1,61 +1,57 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 contract Degen is ERC20, ERC20Burnable, Ownable {
-    constructor
-    (
-        string memory name,
-        string memory symbol,
-        address initialOwner
-    ) 
-    ERC20(name, symbol) Ownable(initialOwner) 
-    {}
 
-    function mint(address to, uint256 amount) public onlyOwner 
-    {
+    event ItemRedeemed(address indexed player, string itemName, uint256 tokensSpent);
+
+    constructor() ERC20("degen", "DGN") Ownable(msg.sender) {}
+        // The Ownable constructor automatically sets msg.sender as the owner, no need to specify explicitly
+    
+
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function balance() external view returns (uint256) 
-    {
-        return this.balanceOf(msg.sender);
+    function getBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
     }
 
-    function tranfer(address to, uint256 value)
-        external
-        returns (string memory)
-    {
-        require(balanceOf(msg.sender) >= value);
-        approve(msg.sender, value);
-        transferFrom(msg.sender, to, value);
-        return "success";
+    function transferTokens(address to, uint256 amount) external returns (string memory) {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(msg.sender, to, amount);  // Direct transfer without unnecessary approval
+        return "Transfer successful";
     }
 
-    function storeinfo() external pure returns (string memory) 
-    {
-        return
-            "1. GAME NFT  100tokens 2. GAME TSHIRT  750 tokens   3. ACCESSORIES 500 tokens";
+    function storeInfo() external pure returns (string memory) {
+        return "1. GAME NFT 100 tokens 2. GAME TSHIRT 750 tokens 3. ACCESSORIES 500 tokens";
     }
 
-    function redeemTokens(uint256 _value) external {
-        require(_value > 0, "Invalid token value");
-        require(balanceOf(msg.sender) >= _value, "Insufficient balance");
-        if (_value == 1) 
-        {
-            _burn(msg.sender, 100);
-        } 
-        else if (_value == 2)
-        {
-            _burn(msg.sender, 750);
-        } 
-        else if (_value == 3) 
-        {
-            _burn(msg.sender, 500);
+    function redeemTokens(uint256 option) external {
+        uint256 tokensRequired;
+        string memory itemName;
+
+        if (option == 1) {
+            tokensRequired = 100;
+            itemName = "GAME NFT";
+        } else if (option == 2) {
+            tokensRequired = 750;
+            itemName = "GAME TSHIRT";
+        } else if (option == 3) {
+            tokensRequired = 500;
+            itemName = "ACCESSORIES";
+        } else {
+            revert("Invalid redeem option");
         }
+
+        require(balanceOf(msg.sender) >= tokensRequired, "Insufficient balance to redeem");
+        _burn(msg.sender, tokensRequired);
+
+        // Emit an event that indicates the item redeemed
+        emit ItemRedeemed(msg.sender, itemName, tokensRequired);
     }
 }
